c2d2969c28152edbc71cde75c4a40d1ff7ee80ec
Michael Baumann <mbaumann@ucsc.edu>
2018-02-27 18:59:51 -0800

    WIP - Added support for loading by reference

--- bin/upload_cloud_bundle_to_dss.py
+++ bin/upload_cloud_bundle_to_dss.py
@@ -2,7 +2,11 @@
 
 """
 Utility to load data files and bundles located in AWS and/or GCP into the HCA Data Storage System (DSS).
+
+The Google controlled access buckets are based on ACLs for user accounts
+Before running this loader, configure use of Google user account, run: gcloud auth login
 """
+import binascii
 import csv
 import json
 import logging
@@ -10,20 +14,23 @@ import sys
 import time
 import typing
 import uuid
-from collections import defaultdict
+from urllib.parse import urlparse
 
+import base64
 import boto3
 import botocore
 import os
 import requests
 from boto3.s3.transfer import TransferConfig
 from cloud_blobstore import BlobStore, s3
+from collections import defaultdict
+from google.cloud.storage import Client
 from hca.dss import DSSClient
-from hca.dss.upload_to_cloud import _mime_type, encode_tags, upload_to_cloud
 from hca.util import SwaggerAPIException
 from io import open
 
 from packages.checksumming_io.checksumming_io import ChecksummingSink, S3Etag
+from upload_to_cloud import upload_to_cloud
 
 logger = logging.getLogger(__name__)
 
@@ -33,6 +40,9 @@ SCHEMA_VERSION = "1.2.1"
 SCHEMA_TYPE = "spinnaker_metadata"
 
 DSS_ENDPOINT_DEFAULT = "https://commons-dss.ucsc-cgp-dev.org/v1"
+
+# Google Cloud Access
+GOOGLE_PROJECT_ID = "platform-dev-178517"  # For requester pays
 
 # Default values for "topmed_open_access" data set
 SOURCE_BUCKET_DEFAULT = "cgp-commons-public"
@@ -58,6 +68,7 @@ class DssUploader:
         self.dry_run = dry_run
         self.s3_client = boto3.client("s3")
         self.blobstore = s3.S3BlobStore(self.s3_client)
+        self.gs_client = Client()
         os.environ.pop('HCA_CONFIG_FILE', None)
         self.dss_client = DSSClient()
         self.dss_client.host = "https://commons-dss.ucsc-cgp-dev.org/v1"
@@ -68,8 +79,67 @@ class DssUploader:
             self._set_hca_metadata_tags(self.s3_client, bucket, key, checksums)
         return self._upload_tagged_cloud_file_to_dss(bucket, key, file_uuid, bundle_uuid)
 
-    def upload_local_file(self, path, bundle_uuid: str):
-        file_uuid, key = self._upload_local_file_to_staging(path)
+    def upload_cloud_file_by_reference(self, filename: str, file_cloud_urls: set, bundle_uuid: str , file_uuid: str) -> tuple:
+        if self.dry_run:
+            logger.info(f"DRY RUN: upload_cloud_file_by_reference: {filename} {str(file_cloud_urls)} {bundle_uuid}")
+        file_reference = self._create_file_reference(file_cloud_urls)
+        return self.upload_dict_as_file(file_reference, filename, bundle_uuid, "application/json; dss-type=fileref")
+
+    def _create_file_reference(self, file_cloud_urls: set) -> dict:
+        s3_metadata = None
+        gs_metadata = None
+        for cloud_url in file_cloud_urls:
+            url = urlparse(cloud_url)
+            bucket = url.netloc
+            key = url.path[1:]
+            if url.scheme == "s3":
+                s3_metadata = self._get_s3_file_metadata(bucket, key)
+            elif url.scheme == "gs":
+                gs_metadata = self._get_gs_file_metadata(bucket, key)
+            else:
+                logger.warning("Unsupported cloud URL scheme: {cloud_url}")
+        return self._consolidate_metadata(file_cloud_urls, s3_metadata, gs_metadata)
+
+    def _get_s3_file_metadata(self, bucket: str, key: str) -> dict:
+        metadata = dict()
+        try:
+            response = self.s3_client.head_object(Bucket=bucket, Key=key, RequestPayer="requester")
+            metadata['content-type'] = response['ContentType']
+            metadata['s3_etag'] = response['ETag']
+            metadata['size'] = response['ContentLength']
+        except botocore.exceptions.ClientError as e:
+            logger.warning(f"Error accessing s3://{bucket}/{key} Exception: {e}")
+        return metadata
+
+    def _get_gs_file_metadata(self, bucket: str, key: str) -> dict:
+        metadata = dict()
+        try:
+            gs_bucket = self.gs_client.bucket(bucket, GOOGLE_PROJECT_ID)
+            blob_obj = gs_bucket.get_blob(key)
+            metadata['content-type'] = blob_obj.content_type
+            metadata['crc32c'] = binascii.hexlify(base64.b64decode(blob_obj.crc32c)).decode("utf-8").lower()
+            metadata['size'] = blob_obj.size
+        except Exception as e:
+            logger.warning(f"Error accessing s3://{bucket}/{key} Exception: {e}")
+        return metadata
+
+    @staticmethod
+    def _consolidate_metadata(file_cloud_urls: set, s3_metadata: dict, gs_metadata: dict) -> dict:
+        consolidated_metadata = s3_metadata.copy()
+        consolidated_metadata.update(gs_metadata)
+        consolidated_metadata['url'] = list(file_cloud_urls)
+        return consolidated_metadata
+
+    def upload_dict_as_file(self, value: dict, filename: str, bundle_uuid: str, content_type=None):
+        file_path = "/tmp/" + filename
+        with open(file_path, "w") as fh:
+            fh.write(json.dumps(value, indent=4))
+        result = self.upload_local_file(file_path, bundle_uuid, content_type)
+        os.remove(file_path)
+        return result
+
+    def upload_local_file(self, path, bundle_uuid: str, content_type=None):
+        file_uuid, key = self._upload_local_file_to_staging(path, content_type)
         return self._upload_tagged_cloud_file_to_dss(self.staging_bucket, key, file_uuid, bundle_uuid)
 
     def load_bundle(self, file_info_list: list, bundle_uuid: str):
@@ -78,17 +148,19 @@ class DssUploader:
             response = self.dss_client.put_bundle(**kwargs)
             version = response['version']
         else:
-            print("DSS put bundle: " + str(kwargs))
+            logger.info("DRY RUN: DSS put bundle: " + str(kwargs))
             version = None
-        return f"{bundle_uuid}.{version}"
+        bundle_fqid = f"{bundle_uuid}.{version}"
+        logger.info(f"Loaded bundle: {bundle_fqid}")
+        return bundle_fqid
 
     @staticmethod
     def get_filename_from_key(key: str):
         return key.split("/")[-1]
 
-    def _upload_local_file_to_staging(self, path: str):
+    def _upload_local_file_to_staging(self, path: str, content_type):
         with open(path, "rb") as fh:
-            file_uuids, key_names = upload_to_cloud([fh], self.staging_bucket, "aws")
+            file_uuids, key_names = upload_to_cloud([fh], self.staging_bucket, "aws", False, content_type)
         return file_uuids[0], key_names[0]
 
     @staticmethod
@@ -105,9 +177,6 @@ class DssUploader:
         s3_client.download_fileobj(bucket, key, checksumming_sink, Config=tx_cfg)
         return checksumming_sink.get_checksums()
 
-    def _get_content_type_from_name(self, key: str) -> str:
-        return _mime_type(self.get_filename_from_key(key))
-
     def _set_hca_metadata_tags(self, s3_client, bucket: str, key: str, checksums: dict) -> None:
         metadata = {
             "hca-dss-s3_etag": checksums["s3_etag"],
@@ -123,12 +192,16 @@ class DssUploader:
     def _upload_tagged_cloud_file_to_dss(self, source_bucket: str, source_key: str, file_uuid: str, bundle_uuid: str,
                                          timeout_seconds=1200):
         source_url = f"s3://{source_bucket}/{source_key}"
-        file_name = self.get_filename_from_key(source_key)
+        filename = self.get_filename_from_key(source_key)
+
+        if self.dry_run:
+            logger.info(f"DRY RUN: _upload_tagged_cloud_file_to_dss: {source_bucket} {source_key} {file_uuid} {bundle_uuid}")
+            return file_uuid, None, filename
 
         request_parameters = dict(uuid=file_uuid, bundle_uuid=bundle_uuid, creator_uid=CREATOR_ID, source_url=source_url)
         if self.dry_run:
-            print("DSS put file: " + str(request_parameters))
-            return file_uuid, None, file_name
+            print("DRY RUN: put file: " + str(request_parameters))
+            return file_uuid, None, filename
         response = self.dss_client.put_file._request(request_parameters)
         file_version = response.json().get('version', "blank")
 
@@ -155,7 +228,7 @@ class DssUploader:
                 raise RuntimeError("File {}: registration FAILED".format(source_url))
             logger.debug("Successfully uploaded file")
 
-        return file_uuid, file_version, file_name
+        return file_uuid, file_version, filename
 
 
 class MetadataFileUploader:
@@ -166,11 +239,8 @@ class MetadataFileUploader:
                   bundle_uuid: str) -> tuple:
         metadata_string = self.dss_uploader.blobstore.get(bucket, key).decode("utf-8")
         metadata = json.loads(metadata_string)
-        metadata['core'] = dict(schema_url=schema_url, schema_version="1.1.1", type="metadata")
-        file_path = "/tmp/" + filename
-        with open(file_path, "w") as fh:
-            fh.write(json.dumps(metadata, indent=4))
-        return self.dss_uploader.upload_local_file(file_path, bundle_uuid)
+        metadata['core'] = dict(schema_url=schema_url, schema_version=schema_version, type="metadata")
+        return self.dss_uploader.upload_dict_as_file(metadata, "metadata.json", bundle_uuid)
 
 
 class BundleUploaderForTopMedOpenAccess:
@@ -230,20 +300,47 @@ class BundleUploaderForTopMed12k:
         self.metadata_cache_bucket = metadata_cache_bucket
         self.metadata_cache_key = metadata_cache_key
 
+    def load_bundle(self, metadata_bucket: str, metadata_key: str, data_files: dict, bundle_uuid: str):
+        file_info_list = self._load_bundle_files(metadata_bucket, metadata_key, data_files, bundle_uuid)
+        return self.dss_uploader.load_bundle(file_info_list, bundle_uuid)
+
     def load_all_bundles(self):
         map_specimen_id_to_cloud_files = self._get_map_specimen_id_to_cloud_files()
         map_specimen_id_to_metadata = self._get_map_specimen_id_to_metadata()
-        match_count = 0
+        bundles_loaded_count = 0
         for specimen_id in map_specimen_id_to_cloud_files.keys():
-            if map_specimen_id_to_metadata.keys().__contains__(specimen_id):
-                match_count += 1
-        print(f"match count: {match_count}")
+            if map_specimen_id_to_metadata.get(specimen_id):
+                bundles_loaded_count += 1
+                metadata_file_key = map_specimen_id_to_metadata[specimen_id]['key']
+                bundle_uuid = map_specimen_id_to_metadata[specimen_id]['json']['specimen'][0]['samples'][0]['analysis'][0]['bundle_uuid']
+                self.load_bundle(self.metadata_bucket, metadata_file_key,
+                                 map_specimen_id_to_cloud_files[specimen_id],
+                                 bundle_uuid)
+        print(f"match count: {bundles_loaded_count}")
+
+    def _load_bundle_files(self, metadata_bucket: str, metadata_key: str, data_files: dict, bundle_uuid: str) \
+            -> typing.List[typing.Dict[str, str]]:
+        file_info_list = []
+        # Load metadata file
+        file_uuid, file_version, filename = \
+            self.metadata_file_uploader.load_file(metadata_bucket, metadata_key, "metadata.json",
+                                                  SCHEMA_URL, SCHEMA_VERSION, SCHEMA_TYPE,
+                                                  bundle_uuid)
+        file_info_list.append(dict(uuid=file_uuid, version=file_version, name=filename, indexed=True))
+
+        # Load data files by reference
+        for filename, file_cloud_urls in data_files.items():
+            file_uuid = str(uuid.uuid4())
+            file_uuid, file_version, filename = self.dss_uploader.upload_cloud_file_by_reference(
+                filename, file_cloud_urls, bundle_uuid, file_uuid)
+            file_info_list.append(dict(uuid=file_uuid, version=file_version, name=filename, indexed=False))
+        return file_info_list
 
     def _get_map_specimen_id_to_cloud_files(self):
         manifest_text = self.dss_uploader.blobstore.get(self.manifest_bucket, self.manifest_key).decode("utf-8")
         reader = csv.reader(manifest_text.split('\n'), delimiter='\t')
         raw_data = [columns for columns in reader]
-        map_specimen_to_files = defaultdict(set)
+        map_specimen_to_files = defaultdict(dict)
         for index, row in enumerate(raw_data[1:]):
             if index < self.manifest_start_index:
                 continue
@@ -254,7 +351,12 @@ class BundleUploaderForTopMed12k:
             logger.info(f"Processing manifest row: {index}: {row}")
             specimen = row[3] # sra_sample_id
             for file_index in range(25, 29):
-                map_specimen_to_files[specimen].add(row[file_index])
+                file_url = row[file_index]
+                filename = file_url.split("/")[-1]
+                if map_specimen_to_files[specimen].get(filename):
+                    map_specimen_to_files[specimen][filename].add(file_url)
+                else:
+                    map_specimen_to_files[specimen][filename] = set([file_url])
         return map_specimen_to_files
 
     def _get_map_specimen_id_to_metadata(self):
@@ -274,7 +376,7 @@ class BundleUploaderForTopMed12k:
                         metadata_string = self.dss_uploader.blobstore.get(self.metadata_bucket, metadata_key).decode("utf=8")
                         metadata_json = json.loads(metadata_string)
                         specimen_id = metadata_json['specimen'][0]['submitter_specimen_id']
-                        map_specimen_id_to_metadata[specimen_id] = metadata_json
+                        map_specimen_id_to_metadata[specimen_id] = dict(key=metadata_key, json=metadata_json)
                 except Exception as e:
                     logger.error(f"Exception occurred processing metadata file: {metadata_key}, {e}, {metadata_string}", exc_info=True)
         self._write_metadata_cache(map_specimen_id_to_metadata)
@@ -369,6 +471,10 @@ def main(argv):
                                    help="Clear the metadata cache.")
     options = parser.parse_args(argv)
 
+    # Clear configured credentials, which are likely for service accouts.
+    os.environ.pop('GOOGLE_APPLICATION_CREDENTIALS', None)
+    os.environ.pop('GOOGLE_APPLICATION_SECRETS', None)
+
     dss_uploader = DssUploader(options.dss_endpoint, options.staging_bucket, options.dry_run)
     metadata_file_uploader = MetadataFileUploader(dss_uploader)
 
@@ -389,7 +495,7 @@ def main(argv):
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
-    logger.setLevel(logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
+    logger.setLevel(logging.INFO)
     suppress_verbose_logging()
     main(sys.argv[1:])
---
+++ bin/upload_to_cloud.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3.6
+
+"""
+Run "pip install crcmod python-magic boto3" to install this script's dependencies.
+
+Copied and modified from GitHub Human Cell Atlas dcp-cli/hca/dss/upload_to_cloud.py
+"""
+import logging
+import mimetypes
+import os
+import uuid
+
+import boto3
+from boto3.s3.transfer import TransferConfig
+
+from packages.checksumming_io.checksumming_io import ChecksummingBufferedReader, S3Etag
+
+
+def encode_tags(tags):
+    return [dict(Key=k, Value=v) for k, v in tags.items()]
+
+
+def _mime_type(filename):
+    type_, encoding = mimetypes.guess_type(filename)
+    if encoding:
+        return encoding
+    if type_:
+        return type_
+    return "application/octet-stream"
+
+
+def _copy_from_s3(path, s3, tx_cfg):
+    bucket_end = path.find("/", 5)
+    bucket_name = path[5: bucket_end]
+    dir_path = path[bucket_end + 1:]
+
+    src_bucket = s3.Bucket(bucket_name)
+    file_uuids = []
+    key_names = []
+    logging.info("Key Names:")
+    for obj in src_bucket.objects.filter(Prefix=dir_path):
+        # Empty files with no name were throwing errors
+        if obj.key == dir_path:
+            continue
+
+        logging.info(obj.key)
+
+        file_uuids.append(str(uuid.uuid4()))
+        key_names.append(obj.key)
+
+    return file_uuids, key_names
+
+
+def upload_to_cloud(file_handles, staging_bucket, replica, from_cloud=False, content_type=None):
+    """
+    Upload files to cloud.
+
+    :param file_handles: If from_cloud, file_handles is a aws s3 directory path to files with appropriate
+                         metadata uploaded. Else, a list of binary file_handles to upload.
+    :param staging_bucket: The aws bucket to upload the files to.
+    :param replica: The cloud replica to write to. One of 'aws', 'gc', or 'azure'. No functionality now.
+    :return: a list of each file's unique key name.
+    """
+    tx_cfg = TransferConfig(multipart_threshold=S3Etag.etag_stride,
+                            multipart_chunksize=S3Etag.etag_stride)
+    s3 = boto3.resource("s3")
+    file_uuids = []
+    key_names = []
+
+    if from_cloud:
+        file_uuids, key_names = _copy_from_s3(file_handles[0], s3, tx_cfg)
+
+    else:
+        destination_bucket = s3.Bucket(staging_bucket)
+        for raw_fh in file_handles:
+            with ChecksummingBufferedReader(raw_fh) as fh:
+                file_uuid = str(uuid.uuid4())
+                key_name = "{}/{}".format(file_uuid, os.path.basename(fh.raw.name))
+                destination_bucket.upload_fileobj(
+                    fh,
+                    key_name,
+                    Config=tx_cfg,
+                    ExtraArgs={
+                        'ContentType': content_type if content_type is not None else _mime_type(fh.raw.name)
+                    }
+                )
+                sums = fh.get_checksums()
+                metadata = {
+                    "hca-dss-s3_etag": sums["s3_etag"],
+                    "hca-dss-sha1": sums["sha1"],
+                    "hca-dss-sha256": sums["sha256"],
+                    "hca-dss-crc32c": sums["crc32c"],
+                }
+
+                s3.meta.client.put_object_tagging(Bucket=destination_bucket.name,
+                                                  Key=key_name,
+                                                  Tagging=dict(TagSet=encode_tags(metadata))
+                                                  )
+                file_uuids.append(file_uuid)
+                key_names.append(key_name)
+
+    return file_uuids, key_names
